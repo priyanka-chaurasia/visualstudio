f8d4080c355bc202dfc5797e7d927ce22e720259
priyanka-chaurasia <42887117+priyanka-chaurasia@users.noreply.github.com>
2018-12-14 01:26:06 +0530

    Add files via upload

---
+++ appp.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <title>{{config('app.name','LARA')}}</title>
+    </head>
+    <body>
+        @yield('content');
+    </body>
+
+</html>
